aa6c9d82c3d5ff04472629f5a4d09ce9a8a7ac84
g13945sm <g13945sm@s30442.tsuda.ac.jp>
2015-05-08 12:02:00 +0900

    ドライバー：依田 ナビゲーター：須田
    1000まで変換可能にしました。

--- src/intoEnglish/IntToEng.java
+++ src/intoEnglish/IntToEng.java
@@ -16,17 +16,28 @@ public class IntToEng {
 
     // 数値を英訳する変換するメソッド
     static String translateEng(int n) {
-    	String[] a={"zero","one","two","three","four","five","six","seven","eight","nine",
+    	String[] a={"","one","two","three","four","five","six","seven","eight","nine",
     			"ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
-    	String[] b={"","","twenty"};
+    	String[] b={"","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"};
     	
     	int tenrank=n/10;
     	int unit=n%10;
-    	if(0<n&&n<20){
+    	int hrank=tenrank/10;
+    	if(n==0){
+    	return "zero";
+    	}else if(0<n&&n<20){    	
     		return a[n];
     	}else if(n<100){
     		return b[tenrank]+" "+a[unit];
+    	}else if(n<120){
+    		return a[hrank]+" hundred "+a[n%100]; 
+    	}else if(n<1000){
+    		return a[hrank]+" hundred "+b[n%100/10]+" "+a[n%100%10];
+    	}else if(n==1000){
+    		return "thousand";
     	}
+    	//a[hrank]で100のくらい。n%100で下２桁。
+    	
     	return "つる";
     }
 }
